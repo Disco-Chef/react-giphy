aa3256c79a3e12a99c1975ba493b81ca1c319688
Nikos Agathos <nicolas.agathos@gmail.com>
2019-06-26 22:15:00 +0200

    added scss and started app component

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
-
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<Hello name="World" />, root);
